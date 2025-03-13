4b0172f500c0de08f9a80d6bd5a528fa6bf5d323
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-13 08:48:48 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-        <div className='lg:h-[110vh] md:h-[93vh]  bg-[#DCEDF8] pt-[133px] py-6 w-[100%]'> 
-            <div className='border-blue-900 gap-18 lg:gap-35 xl:gap-18 lg:flex w-3/4 mx-auto'>
+        <div className='  bg-[#DCEDF8] pb-24 pt-[133px] py-6 w-[100%]'> 
+            <div className='border-blue-900 gap-18 lg:gap-35 xl:gap-18 lg:flex w-29/30 sm:w-3/4 mx-auto'>
             <div className='lg:w-1/2 w-full flex  flex-col justify-center '>
-<h1 className='font-bold font-[Plus-Jakarta-Sans] lg:w-[600px] flex justify-center items-center text-[35px]  pl-0 lg:pl-6 w-full'>Online coaching that delivers results</h1>
-<p className='font-bold pt-12 pl-0 flex lg:flex-none justify-center lg:pl-6 text-[#494A4A]'>Explore our online courses</p>
+<h1 className='font-bold font-[Plus-Jakarta-Sans] lg:w-[600px] flex justify-center items-center text-[22px] pt-6 sm:pt-0 sm:text-[35px]  pl-0 lg:pl-6 w-full'>Online coaching that delivers results</h1>
+<p className='font-bold pt-3 sm:pt-12 pl-0 flex lg:flex-none justify-center lg:pl-6 text-[#494A4A]'>Explore our online courses</p>
 <div className='lg:flex flex justify-center  sm:pl-6 pt-4  gap-2'>
 <span className='border-2 text-[13px] font-bold text-center self-center border-[#0555B1] cursor-pointer px-5 py-2.5 rounded-3xl'>NEET</span>
 <span className='border-2 border-[#0555B1]  text-[13px] font-bold text-center cursor-pointer self-center px-5 py-2.5 rounded-3xl'>JEE</span>
@@ -28,3 +28,4 @@ const Hero = () => {
 }
 
 export default Hero
+
