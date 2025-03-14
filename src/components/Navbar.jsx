aeba0757d3f0337d48645be72dd09061824b4afa
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-14 21:12:03 +0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -162,7 +162,7 @@ Login
      
 
 
-      <div className="bg-white flex sm:hidden py-6 px-6 justify-between items-center w-full">
+      <div className="bg-white flex sm:hidden py-4 px-6 justify-between items-center w-full">
         <div className="w-1/2 flex gap-3  items-center">
           <GiHamburgerMenu className="text-[24px]" onClick={handleOpen}/>
           <svg
